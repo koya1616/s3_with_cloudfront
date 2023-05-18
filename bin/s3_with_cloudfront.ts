7a5d454a024450f11f676865356961ff24e15f74
koya1616 <aoyamakoya@team-lab.com>
2023-05-18 22:28:25 +0900

    s3-with-cloudfront

--- bin/s3_with_cloudfront.ts
+++ bin/s3_with_cloudfront.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { S3WithCloudfrontStack } from '../lib/s3_with_cloudfront-stack';
 
 const app = new cdk.App();
@@ -15,7 +15,7 @@ new S3WithCloudfrontStack(app, 'S3WithCloudfrontStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // env: { account: '844602212051', region: 'ap-northeast-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/s3_with_cloudfront-stack.ts
+++ lib/s3_with_cloudfront-stack.ts
@@ -1,16 +1,35 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class S3WithCloudfrontStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create an S3 bucket
+    const bucket = new s3.Bucket(this, 'YourBucket', {
+      versioned: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3WithCloudfrontQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create an OAI for restricting bucket access through CloudFront only
+    const oai = new cloudfront.OriginAccessIdentity(this, 'YourOai', {
+      comment: 'OAI for your Cloudfront distribution',
+    });
+
+    // Grant read access to the OAI
+    bucket.grantRead(oai);
+
+    // Create a CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'YourDistribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(bucket, {originAccessIdentity: oai}),
+        cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED, // Set this to the desired caching policy
+      },
+    });
+
+    // Output the distribution domain name and the bucket domain name
+    new cdk.CfnOutput(this, 'CloudFrontDomain', { value: distribution.distributionDomainName });
+    new cdk.CfnOutput(this, 'BucketDomain', { value: bucket.bucketDomainName });
   }
 }
